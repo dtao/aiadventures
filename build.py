7990cdd8fa9e6f79557ffcba5726356fc3ca3aed
Dan Tao <daniel.tao@gmail.com>
2023-04-05 23:25:00 -0500

    Switch to build-time rendering of pages
    I'm not ecstatic about going this route, but I was able to get it
    working relatively quickly, and it's what I'm familiar/comfortable with.
    
    The idea here is to use a service like Netlify that can, with minimal
    setup required, basically run build.py and render the entire site as
    static content.

---
+++ build.py
@@ -0,0 +1,55 @@
+import glob
+import os
+
+from pathlib import Path
+
+# Used for parsing YAML frontmatter
+import frontmatter
+
+# Used for rendering final HTML from template
+import jinja2
+
+# Used for parsing Markdown content
+import marko
+
+# Get the base template that we'll use for every post.
+with open('templates/summary.html') as f:
+    template_content = f.read()
+
+# Ensure the output directory exists.
+Path('site/summaries').mkdir(parents=True, exist_ok=True)
+
+# Produce a rendered HTML file for each summary.
+summaries = []
+for p in glob.glob('summaries/*/*.md'):
+    filename = os.path.basename(p)
+    slug, _ = os.path.splitext(filename)
+    summary = frontmatter.load(p)
+    template = jinja2.Template(template_content)
+    output = template.render({
+        'title': summary['summary'],
+        'content': marko.convert(summary.content)
+    })
+    with open(f'site/summaries/{slug}.html', 'w+') as f:
+        f.write(output)
+
+    summaries.append({
+        'slug': slug,
+        'title': summary['summary']
+    })
+
+# Produce the rendered index page.
+with open('templates/index.html') as f:
+    template_content = f.read()
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('site/index.html', 'w+') as f:
+    template = jinja2.Template(template_content)
+    output = template.render({
+        'readme': marko.convert(readme),
+        'summaries': summaries
+    })
+    with open(f'site/index.html', 'w+') as f:
+        f.write(output)
