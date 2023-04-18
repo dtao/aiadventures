730dab9d22107b6b984884908e10313a3f5af306
Dan Tao <daniel.tao@gmail.com>
2023-04-17 23:29:36 -0500

    Add new type of content: "Thoughts"

--- build.py
+++ build.py
@@ -16,17 +16,19 @@ import marko
 
 markdown = marko.Markdown(extensions=['gfm'])
 
-# Get the base template that we'll use for every post.
-with open('templates/summary.html') as f:
-    template_content = f.read()
-
 # Ensure the output directory exists for summary pages.
 Path('site/summaries').mkdir(parents=True, exist_ok=True)
+
+# Ensure the output directory exists for 'thoughts'.
+Path('site/thoughts').mkdir(parents=True, exist_ok=True)
 
 # Ensure the output directory exists for images.
 Path('site/images').mkdir(parents=True, exist_ok=True)
 
 # Produce a rendered HTML file for each summary.
+with open('templates/summary.html') as f:
+    template_content = f.read()
+
 summaries = []
 for p in glob.glob('summaries/*/*'):
     filename = os.path.basename(p)
@@ -61,6 +63,38 @@ for p in glob.glob('summaries/*/*'):
     elif ext in ('.gif', '.jpg', '.jpeg', '.png'):
         shutil.copyfile(p, f'site/images/{filename}')
 
+# Produce a rendered HTML file for each 'thought'.
+with open('templates/thought.html') as f:
+    template_content = f.read()
+
+thoughts = []
+for p in glob.glob('thoughts/*'):
+    filename = os.path.basename(p)
+    slug, ext = os.path.splitext(filename)
+
+    if ext == '.md':
+        thought = frontmatter.load(p)
+        title_date = datetime.strftime(thought['date'], '%b %-d, %Y')
+        template = jinja2.Template(template_content)
+        output = template.render({
+            'slug': slug,
+            'title': thought['summary'],
+            # The frontmatter library strips out the trailing newline character;
+            # add it back in to ensure links are rendered properly.
+            'content': markdown.convert(thought.content + '\n'),
+            'title_date': title_date,
+            'date': thought['date']
+        })
+        with open(f'site/thoughts/{slug}.html', 'w+') as f:
+            f.write(output)
+
+        thoughts.append({
+            'slug': slug,
+            'title': thought['summary'],
+            'title_date': title_date,
+            'date': thought['date']
+        })
+
 # Produce the rendered index page.
 with open('templates/index.html') as f:
     template_content = f.read()
@@ -72,7 +106,8 @@ with open('site/index.html', 'w+') as f:
     template = jinja2.Template(template_content)
     output = template.render({
         'readme': markdown.convert(readme),
-        'summaries': sorted(summaries, key=lambda s: s['date'], reverse=True)
+        'summaries': sorted(summaries, key=lambda s: s['date'], reverse=True),
+        'thoughts': sorted(thoughts, key=lambda t: t['date'], reverse=True)
     })
     with open(f'site/index.html', 'w+') as f:
         f.write(output)
